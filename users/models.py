a1a799c760d2cee79f08db7edfcf935c885a1574
DiklaS <diklasternx@gmail.com>
2025-04-17 23:01:02 +0300

    added solo availability and group avilability

--- users/models.py
+++ users/models.py
@@ -1,5 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from datetime import date
+
+DAYS_OF_WEEK = [
+    ('sun', 'Sunday'),
+    ('mon', 'Monday'),
+    ('tue', 'Tuesday'),
+    ('wed', 'Wednesday'),
+    ('thu', 'Thursday'),
+    ('fri', 'Friday'),
+    ('sat', 'Saturday'),
+]
 
 class CustomUser(AbstractUser):
     pass
+
+class SoloAvailability(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    week_start = models.DateField(help_text="Sunday of the relevant week")
+    day = models.CharField(max_length=3, choices=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        unique_together = ('user', 'week_start', 'day', 'start_time')
+ 
+        
+class GroupAvailability(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    week_start = models.DateField(help_text="Sunday of the relevant week")
+    day = models.CharField(max_length=3, choices=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        unique_together = ('user', 'week_start', 'day', 'start_time')
